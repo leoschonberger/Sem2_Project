e258531c4f6ab5d2e49fe3b96095cc3312c4b485
Leo Schonberger <leoschonberger@gmail.com>
2019-01-25 11:22:01 -0800

    Added actual java project

--- src/Sem2Main.java
+++ src/Sem2Main.java
@@ -6,28 +6,26 @@ public class Sem2Main {
 
     //Main Method
     public static void main(String[] args) {
-        Sem2Main = new Sem2Main
+        new Sem2Main();
+    }
 
-}
+    //Constructor
+    private Sem2Main() {
+        leo();
+        jackson();
+        eddie();
+    }
 
-//Constructor
-private Sem2Main{
-        new leo();
-        new jackson();
-        new eddie();
-
-        }
-
-private void leo(){
+    private void leo() {
         System.out.println("leo");
-        }
+    }
 
-    private void jackson(){
+    private void jackson() {
         System.out.println("Good evening gamerz it's Jackson");
     }
 
     //This code made by fast ed
-    private void eddie(){
+    private void eddie() {
         System.out.println("Hey, VSAUCE, Eddie here");
     }
 }
