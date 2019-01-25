b9ee24d9a8515c276a5ce31634d88db03e275a05
Leo Schonberger <leoschonberger@gmail.com>
2019-01-25 10:55:30 -0800

    Started structure

--- Sem2Main.java
+++ Sem2Main.java
@@ -1,6 +1,32 @@
 //https://discord.gg/X46Bte
 
 
-public static void main(String[]args){
+//Class header
+public class Sem2Main {
 
-        }
+    //Main Method
+    public static void main(String[] args) {
+        Sem2Main = new Sem2Main
+
+    }
+
+    //Constructor
+    private Sem2Main{
+        new leo();
+        new jackson();
+        new eddie();
+
+    }
+
+    private void leo(){
+        System.out.println("leo");
+    }
+
+    private void jackson(){
+
+    }
+
+    private void eddie(){
+
+    }
+}
